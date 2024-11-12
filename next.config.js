2b13e495552185d5a0932f50b38eb3d4c6b31696
Gasim Gasimzada <gasim@gasimzada.net>
2024-11-12 21:32:31 +0100

    Update export script

--- next.config.js
+++ next.config.js
@@ -1,24 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
-   webpack: function (config) {
-    config.module.rules.push(
-      {
-        test: /\.ya?ml$/,
-        use: 'js-yaml-loader',
-      },
-    )
-    return config
+  webpack: function (config) {
+    config.module.rules.push({
+      test: /\.ya?ml$/,
+      use: "js-yaml-loader",
+    });
+    return config;
   },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/posts',
-        permanent: false,
-      },
-    ]
-  },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
